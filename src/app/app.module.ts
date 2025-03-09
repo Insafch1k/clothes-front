030bb063b9a97a6b953edba6abd3bb0893eb215e
Айназ <ainaz.dulmaran@yandex.ru>
2025-03-09 15:57:45 +0300

    Добавить страницы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { WardrobeComponent } from './pages/wardrobe/wardrobe.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CatalogComponent,
+    WardrobeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
